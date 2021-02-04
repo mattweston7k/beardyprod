b514b864023a9179b0314a945de64a66938c2603
mattweston7k <a6561552@sina.com>
2021-02-05 01:19:18 +0800

    More markers & pics added

--- mobile-demo/scripts/content/locations.js
+++ mobile-demo/scripts/content/locations.js
@@ -1,6 +1,7 @@
 var pf = "scene-"
 var locations = [
   ["Longitude","Latitude","Place","Description"],
+  
   ["24.98443","60.14054","Suomenlinna 1",
 	"Suomenlinna cannons", "1000px-tester.jpg"],
 	
@@ -8,6 +9,24 @@ var locations = [
 	"Suomenlinna water reserve", pf+"1.jpg"],
 	
   ["25.10991","60.20908","Puotila",
-	"Puotila beach", pf+"3.jpg"]
-  
+	"Puotila beach", pf+"3.jpg"],
+  /*
+  ["24.95533", "60.17293", "Studio",
+	"music making and such", pf+"2.jpg"],
+*/
+
+["24.93023", "60.16758", "Kamppi", "Sauna bus, for drinking and partying", "kamppi-sauna.jpg"],
+
+	["25.09836", "60.19825", "Marjaniemi", "for winter ice skating", "ice-2.jpg"],
+	
+		
+	["-1.527314", "53.89689", "Harewood house", "Medieval fair", "harewood-house-1.jpg"],
+	
+	
+	
+//easter "eggs"	
+	["139.70355", "35.69384", "Tokyo", "Shinjuku street temple", "tokyo.jpg"],	
+	["135.72924", "35.03937", "Kyoto, Kinkaku-ji", "Kinkaku-ji", "kyoto.jpg"]
+
+	
 ];
