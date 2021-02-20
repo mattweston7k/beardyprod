d909531ac1efb21b6e6f3a902af4ac3e699c0c36
mattweston7k <a6561552@sina.com>
2021-02-20 17:59:43 +0800

    update

--- mobile-demo/scripts/content/locations.js
+++ mobile-demo/scripts/content/locations.js
@@ -1,22 +1,21 @@
-
 var locations = [
   ["Longitude","Latitude","Single locations","Description"],
 //2014  
   ["24.98443","60.14054","Suomenlinna 1",
-	"Suomenlinna cannons", "suokki-1.jpg"],
+	"Suomenlinna cannons", "suokki-1.JPG"],
 	
   ["24.98590","60.13810","Suomenlinna 2",
-	"Suomenlinna water reserve", "suokki-2.jpg"],
+	"Suomenlinna water reserve", "suokki-2.JPG"],
 
-	["24.98389","60.14927","Suomenlinna 3", "Ferry journey begins...", "suokki-3.jpg"],
+	["24.98389","60.14927","Suomenlinna 3", "Ferry journey begins...", "suokki-3.JPG"],
 	
   ["25.10991","60.20908","Puotila",
 	//"Puotila beach", pf+"3.jpg"],
-	"Puotila beach", "puotila-beach.jpg"],
+	"Puotila beach", "puotila-beach.JPG"],
 			
-	["24.95323","60.17277","Kruunuhaka", "Police slow riding on a horse", "krunika-1.jpg"],
+	["24.95323","60.17277","Kruunuhaka", "Police slow riding on a horse", "krunika-1.JPG"],
 	
-	["24.94186","60.16889","Aleksanterinkatu 21", "Viking restaurant Harald. Reindeer heart on the plate.", "harald-1.jpg"],
+	["24.94186","60.16889","Aleksanterinkatu 21", "Viking restaurant Harald. Reindeer heart on the plate.", "harald-1.JPG"],
 	
 	
 //older  
@@ -31,7 +30,6 @@ var locations = [
 	["139.70355", "35.69384", "Tokyo", "Shinjuku street temple", "tokyo.jpg"],	
 	["135.72924", "35.03937", "Kyoto, Kinkaku-ji", "Kinkaku-ji", "kyoto.jpg"]
 
-	
 ];
 
 //use: http://universimmedia.pagesperso-orange.fr/geo/loc.htm
