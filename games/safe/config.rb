df9bf754d917b0e7e77a4444b96330c2ab001320
mattweston7k <a6561552@sina.com>
2021-06-17 17:31:36 +0800

    Safe open game prototype
    Prototype for rotating image with slider and firing function call when
    random generated correct val is hit

---
+++ games/safe/config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/scripts'
+relative_assets = true
+line_comments = true
